149f60d3a680b0b3f7ecdd78e8702620c819f110
Mikhail Chaus <setting.chaus@gmail.com>
2022-06-07 19:57:33 +0200

    Initial Commit

---
+++ marvelgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvelgram
+//
+//  Created by Mikhail Chaus on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
