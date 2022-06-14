96735fd38a63702858e329ac7b5aa8f12ce4d2dc
Mikhail Chaus <setting.chaus@gmail.com>
2022-06-14 23:28:34 +0200

    Adding Localizable strings.

---
+++ marvelgram/Supporting Files/Localizable/LocalizableStrings.swift
@@ -0,0 +1,18 @@
+//
+//  LocalizableStrings.swift
+//  marvelgram
+//
+//  Created by Mikhail Chaus on 14.06.2022.
+//
+
+import Foundation
+
+enum LocalizableStrings: String {
+    case exploreMoreTitle = "exploreMore.title"
+    
+    // MARK: - localizedString
+    
+    var localizedString: String {
+        return NSLocalizedString(self.rawValue, comment: "")
+    }
+}
